c12f808fe83ff29cb4a96dd7ef4151e74ae741df
abouchereau <anthonybouchereau@hotmail.com>
2024-01-17 10:10:55 +0100

    options

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const electron = require('electron');
 const path = require('path');
 
 const { app, BrowserWindow, Menu} = electron; //Menu generated for the purposes of visiting new URLs.
-const prompt = require('electron-prompt'); //prompting system to get user URL
 
 
 let pluginName = null; //put the right flash plugin in depending on the operating system.
@@ -39,68 +38,14 @@ switch (process.platform) {
 		break
 }
 app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName));
-//CACHE IS ENABLED but restarted on launch!
-//TODO: Support importing custom .swf files to play
-
-let mainWindow;
-
-function promptIt(isSplash, win) { //function to handle prompting
-	prompt({
-		title: "Flash Browser",
-		label: "URL: ",
-		inputAttrs: {
-			type: 'url'
-		},
-		type: 'input'
-	})
-		.then((r) => {
-			if (r == null) {
-				console.log("user cancelled!");
-				if (!isSplash) {
-					app.quit();
-				}
-			} else {
-				console.log('result', r);
-				if (isSplash) {
-					win.loadURL(r);
-				}
-				else {
-					win.loadURL(r);
-					win.maximize();
-					setTimeout(() => {
-						win.show(); //pauses program for 1 second to allow web-page to load before rendering.
-					}, 1000);
-                }
-
-			}
-		}).catch(console.error)
-}
-
 
 app.on('ready', function () {
-	/*var menu = Menu.buildFromTemplate([
-		{
-			label: 'Menu',
-			submenu: [
-				{
-					label: 'Change URL',
-					click() {
-						promptIt(true, win);
-                    }
-				},
-				{
-					label: 'Exit',
-					click() {
-						app.quit();
-                    }
-				}
-			]
-		}
-	])
-	Menu.setApplicationMenu(menu);*/
-	
 	let win = new BrowserWindow({
 		show: false,
+		//fullscreen: true,
+		skipTaskbar: true,
+		autoHideMenuBar: true,
+		titleBarStyle: 'hidden',
 		webPreferences: {
 			plugins: true
 		}
@@ -113,7 +58,6 @@ app.on('ready', function () {
 			//clearCache
 		});
 	}, 1000);
-	//promptIt(false, win);
 
 	
 })
